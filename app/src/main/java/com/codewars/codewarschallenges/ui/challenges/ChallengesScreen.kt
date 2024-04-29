371054522f6fd062d305415c0351bbea96d45cbc
Yurii Hetsko <getttzzz@gmail.com>
2024-04-29 22:27:08 +0200

    feat: add basic error handling for pagination

--- app/src/main/java/com/codewars/codewarschallenges/ui/challenges/ChallengesScreen.kt
+++ app/src/main/java/com/codewars/codewarschallenges/ui/challenges/ChallengesScreen.kt
@@ -1,5 +1,6 @@
 package com.codewars.codewarschallenges.ui.challenges
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,11 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.LoadState.Loading
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -53,6 +57,11 @@ fun ChallengesScreen(
             val lazyColumnState: LazyListState = rememberLazyListState()
             val challenges: LazyPagingItems<Challenge> = viewModel.challenges.collectAsLazyPagingItems()
 
+            viewModel.error?.let {
+                Toast.makeText(LocalContext.current, it.message, Toast.LENGTH_SHORT).show()
+                viewModel.onErrorConsumed()
+            }
+
             LazyColumn(
                 state = lazyColumnState,
                 contentPadding = PaddingValues(all = 8.dp),
@@ -69,6 +78,16 @@ fun ChallengesScreen(
                 }
                 if (challenges.loadState.append is Loading) {
                     item { CircularProgressHorizontallyCentered() }
+                }
+            }
+            LaunchedEffect(key1 = challenges.loadState) {
+                when {
+                    challenges.loadState.refresh is LoadState.Error -> {
+                        viewModel.onError((challenges.loadState.refresh as LoadState.Error).error)
+                    }
+                    challenges.loadState.append is LoadState.Error -> {
+                        viewModel.onError((challenges.loadState.append as LoadState.Error).error)
+                    }
                 }
             }
         }
--- app/src/main/java/com/codewars/codewarschallenges/ui/challenges/ChallengesViewModel.kt
+++ app/src/main/java/com/codewars/codewarschallenges/ui/challenges/ChallengesViewModel.kt
@@ -1,12 +1,29 @@
 package com.codewars.codewarschallenges.ui.challenges
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.codewars.codewarschallenges.ui.challenges.paging.ChallengesPager
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
 class ChallengesViewModel(challengesPager: ChallengesPager) : ViewModel() {
 
-    val challenges = challengesPager.pagingDataFlow.cachedIn(scope = viewModelScope)
+    val challenges = challengesPager.pagingDataFlow
+        .catch { error -> onError(error) }
+        .cachedIn(viewModelScope)
 
+    var error by mutableStateOf<Throwable?>(value = null)
+        private set
+
+    fun onError(throwable: Throwable) {
+        viewModelScope.launch { error = throwable }
+    }
+
+    fun onErrorConsumed() {
+        viewModelScope.launch { error = null }
+    }
 }
